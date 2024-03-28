e161be857c958985e2661125d1657da2fb3e7744
viniciusuliano <vgarciauliano@gmail.com>
2024-03-28 14:45:55 -0300

    commit exercicio de ordenação de pares e impares

---
+++ ex001.js
@@ -0,0 +1,13 @@
+let array = [3,1,2,4,6,5]
+for(let i =0;i < array.length; i++){
+  if(array[i] % 2 ==0){
+    console.log(array[i])
+  }
+}
+
+
+for(let i =0;i<array.length; i++){
+  if(array[i] % 2 == 1){
+    console.log(array[i])
+  }
+}
